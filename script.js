25d4bf27e91020d8b359a67ddc14ac452c50e3b4
ChristineWest <christinewwwest@gmail.com>
2022-07-08 10:43:20 -0400

    took test script out of HTML and put in proper script.js file

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-// select image as the target and store them in variables
-const coruscant = document.querySelector('.coruscant img');
-const degobah = document.querySelector('.degobah img');
-const dqar = document.querySelector('.dqar img');
-const naboo = document.querySelector('.naboo img');
-const tattoine = document.querySelector('.tattoine img');
+// select image as the target for DOM node manipulation and store them in variables
+    const coruscant = document.querySelector('#coruscant img');
+    const degobah = document.querySelector('#degobah img');
+    const dqar = document.querySelector('#dqar img');
+    const naboo = document.querySelector('#naboo img');
+    const tatooine = document.querySelector('#tatooine img');
+   
+// select planets parent div and store it in variables
+    const coruscantDiv = document.getElementById('coruscant');
+    const degobahDiv = document.getElementById('debobah');
+    const dqarDiv = document.getElementById('dqar');    
+    const nabooDiv = document.getElementById('naboo');
+    const tatooineDiv = document.getElementById('tatooine');
 
-coruscant.addEventListener('click', function () {
-    console.log('hit');
+    // create an explosion image element and store it in a variable
+    const explosionCoruscant = document.createElement('img');
+    const explosionDegobah = document.createElement('img');
+    const explosionDqar = document.createElement('img');
+    const explosionNaboo = document.createElement('img');
+    const explosionTatooine = document.createElement('img');
+    // addEventListener to coruscant image
+    coruscant.addEventListener('click', function () {
+        // use annonymous function to change coruscant image to display none
+        coruscant.style.display = 'none';
+        // setAttribute as "name-value" pair to the explosion image element to give this image a path
+        explosionCoruscant.setAttribute( 'src', './assets/explosion.jpg');
+
+    // insert the explosion image now with the path to a jpg image in assets folder to the parent element in HTML       
+    coruscantDiv.appendChild(explosionCoruscant);
 })
 
-// create eventHandle for clicking on the img
-function handleClick(eventObject) {
-    console.log(eventObject.target);
+// repeat the same process for the other planets - this is not the DRY code but would make it MVP
+degobah.addEventListener('click', function () {
+    degobah.style.display = 'none';
+    explosionDegobah.setAttribute('src', './assets/explosion-degobah.jpg');
+    degobahDiv.appendChild(explosionDegobah);
+})
 
-    // remove the plante image
-    eventObject.target.style.visibility = 'hidden';
+// DRY code attempt
+// select elements with class name of planet store them in an object
 
-    // create the img element, assign image from assets folder to the img element, insert the new img in the div parent of the original img
-    const explosion = document.createElement('img');
-    explosion.src = './assets/explosion.jpg';
-    eventObject.parentElement.appendChild(explosion);
-}
-
-coruscant.addEventListener('click', handleClick);
-degobah.addEventListener('click', handleClick);
-dqar.addEventListener('click', handleClick);
-naboo.addEventListener('click', handleClick);
-tattoine.addEventListener('click', handleClick);
